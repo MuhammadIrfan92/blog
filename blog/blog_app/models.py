c79bf8bebd691644fd165470cf316b76a179baa6
Irfan <irfanabbas034@gmail.com>
2021-05-27 09:32:48 +0500

    created admin panel and stuff

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 # Create your models here.
+
+STATUS  = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length= 200, null= True)
+    slug = models.SlugField(max_length= 200, null= True)
+    author = models.ForeignKey(User, on_delete=CASCADE, related_name='blog_posts',null=True)
+    updated_on = models.DateTimeField(auto_now=True,null=True)
+    content = models.TextField(null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS,default=0,null=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
