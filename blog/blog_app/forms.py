9ca38ac72ed3a389eec5a8b31904147e542e05b2
Irfan <irfanabbas034@gmail.com>
2021-06-17 23:39:43 +0500

    ajax implementation working

--- blog/blog_app/forms.py
+++ blog/blog_app/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Post
 
+CHOICES= [
+    ('fashion', 'fashion'),
+    ('politics', 'politics'),
+    ('geography', 'geography'),
+    ('sports', 'sports'),
+    ]
 class createajax(forms.ModelForm):
+
     class Meta:
         model = Post
         fields = ['title','author','content','category']
